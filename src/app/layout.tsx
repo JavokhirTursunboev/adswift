35fc4ccd1eec1a87817a64e5d9ced6e6bb7aec0f
Javokhir <jtursunboev@bk.ru>
2024-01-27 22:19:30 +0900

    Add new files and update existing files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavbarMain from "@/components/Navbar/Navbar";
+import { Providers } from "./Nextui/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <div className="container mx-[30px] md:m-auto">
+            <NavbarMain />
+            {children}
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col lg:flex-row item lg:items-center justify-around gap-[30px] mt-[90px]">
+      <div className="flex flex-col gap-[3rem] items-start lg:max-w-[450px]">
+        <h1 className="text-[2.5rem] md:text-[60px] leading-[40px] md:leading-[55px]  font-bold">
+          Build beautiful website & mobile apps.
+        </h1>
+        <p className="text-[16px]]">
+          Create live segments and target the right people for messages based on their behaviors.
+        </p>
+        <button className="bg-[#5ab424] p-[20px] text-white rounded-[20px] hover:bg-blue-600">
+          Get Started
+        </button>
+      </div>
+      <div className="relative mt-[50px]">
+        <Image src="/k5c.png" width={600} height={600} alt="main " className=" absolute z-10" />
+        <Image src="/web4.png" width={600} height={600} alt="main" className=" z-0" />
+      </div>
+    </div>
+  );
 }
