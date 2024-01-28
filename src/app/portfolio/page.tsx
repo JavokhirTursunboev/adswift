eac3c6c1792b2457f62b6d913dbd8de04e654694
Javokhir <jtursunboev@bk.ru>
2024-01-28 20:41:13 +0900

    Navbar is done i added usePathname

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,4 +1,14 @@
-
+import Card from "@/components/Portfolio/Card";
+import { projects } from "@/fakedata/data";
+import { ReactNode } from "react";
+import styles from "./page.module.css";
+import CardForMobile from "@/components/Portfolio/CardForMobile";
 export default function page() {
-  return <div>Portfolio</div>;
+  return (
+    <main className="mt-[50px] mb-[100vh]">
+      {projects.map((project, index): ReactNode => {
+        return <Card key={index} {...project} />;
+      })}
+    </main>
+  );
 }
