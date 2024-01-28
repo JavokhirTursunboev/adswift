b66cf0186d571456f300987d8382b9d9fd2f9122
Javokhir <jtursunboev@bk.ru>
2024-01-29 01:28:23 +0900

    Fix bug in login functionality

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,42 @@
+"use client";
 import Card from "@/components/Portfolio/Card";
 import { projects } from "@/fakedata/data";
-import { ReactNode } from "react";
+import { useScroll } from "framer-motion";
+import { ReactNode, useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
 
-export default function page() {
+export default function Page() {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  // !smooth sroll
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  });
   return (
-    <main className="mt-[50px] mb-[100vh] my-auto">
-      {projects.map((project, index): ReactNode => {
-        return <Card key={index} {...project} />;
+    <main ref={container} className="mt-[50px] mb-[100vh] my-auto">
+      {projects.map((project, i): ReactNode => {
+        const targetScale = 1 - (projects.length - i) * 0.05;
+
+        return (
+          <Card
+            key={`p_${i}`}
+            i={i}
+            progress={scrollYProgress}
+            {...project}
+            range={[i * 0.25, 1]}
+            targetScale={targetScale}
+          />
+        );
       })}
     </main>
   );
--- src/components/Portfolio/Card.tsx
+++ src/components/Portfolio/Card.tsx
@@ -5,38 +5,55 @@ interface CardProps {
   src: string;
   link: string;
   color: string;
+  i?: number;
+  range?: number[];
+  targetScale?: number;
+  progress?: MotionValue<number>;
 }
 import { MotionValue, motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
 
-export default function Card({ title, description, src, link, color }: CardProps) {
+export default function Card({
+  i,
+  title,
+  description,
+  src,
+
+  color,
+  progress,
+  range,
+  targetScale,
+}: CardProps) {
   const container = useRef(null);
 
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start end", "start start"],
   });
-  const scale: MotionValue<number> = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  const scale = useTransform(progress, range, [1, targetScale]);
   return (
     <div
       ref={container}
-      className="h-[100vh] w-[90%] md:w-full mx-auto flex  items-center justify-center sticky top-0 "
+      className="h-[100vh] w-[90%] md:w-full
+       mx-auto flex  items-center justify-center
+        sticky top-0 "
     >
-      <div
-        style={{ backgroundColor: color }}
+      <motion.div
+        style={{ scale, backgroundColor: color, top: `calc(-5vh + ${i * 25}px)` }}
         className="bg-[color]
          flex
-         flex-col relative lg:top-[-10] top-[-15%]
+         flex-col relative lg:top-[-10] top-[-10%]
           h-[500px] max-w-[1000px]
            rounded-[25px] p-[20px] lg:p-[50px]
             transform-origin-top"
       >
         {/* Image for Phone */}
         <div className=" md:hidden   relative w-full h-full rounded-[25px] overflow-hidden">
-          <div className="w-full h-full">
+          <motion.div style={{ scale: imageScale }} className="w-full h-full">
             <Image fill src={`/images/${src}`} alt="iamges" className="object-cover" />
-          </div>
+          </motion.div>
         </div>
         <h2 className="text-center text-[22px] lg:text-[28px] mt-[20px] ">{title}</h2>
         {/* body */}
@@ -44,19 +61,19 @@ export default function Card({ title, description, src, link, color }: CardProps
           <div className=" w-full lg:w-[40%] relative top-[10%]">
             <p className="text-[13px] lg:text-[16px]">{description}</p>
             <span className="flex items-center gap-[5px] ">
-              <a href={link} target="_blank" className="text-[12px] underline cursor-pointer">
+              <a href="#" target="_blank" className="text-[12px] underline cursor-pointer">
                 See more
               </a>
             </span>
           </div>
 
           <div className="hidden md:block relative w-[60%] h-full rounded-[25px] overflow-hidden">
-            <motion.div style={{ scale }} className="w-full h-full">
+            <motion.div style={{ scale: imageScale }} className="w-full h-full">
               <Image fill src={`/images/${src}`} alt="iamges" className="object-cover" />
             </motion.div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
