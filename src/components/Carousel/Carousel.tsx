bc18e3ae5de6c1bca8014bbfd68f8c3d0a828ee9
Javokhir <jtursunboev@bk.ru>
2024-02-03 10:40:51 +0900

    Add Swiper.js and framer.animation

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -12,19 +12,31 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import Image from "next/image";
 import { dataCarousel } from "./dataCarousel";
 import GlobalBtn from "../GlobalComponents/button";
+import { animate, useMotionValue, useTransform, motion } from "framer-motion";
 
 // swiper
 export default function CarouselParent() {
+  // animation for number
+  const count = useMotionValue(0);
+  const countNumPer = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const roundedNumPer = useTransform(countNumPer, (latest) => Math.round(latest));
+  useEffect(() => {
+    const controlsNum = animate(count, dataCarousel[0].num);
+    const controlsNumPer = animate(countNumPer, dataCarousel[0].numPer);
+    return () => {
+      controlsNum.stop();
+      controlsNumPer.stop();
+    };
+  }, [count, countNumPer]);
+
   return (
     <div className=" container flex w-full mx-auto px-[20px]  py-[80px]   m-auto">
       <div className="w-full lg:max-w-[550px] ">
         <Swiper
           spaceBetween={30}
           centeredSlides={true}
-          // autoplay={{
-          //   delay: 2500,
-          //   disableOnInteraction: false,
-          // }}
+          autoplay={false}
           pagination={{
             clickable: true,
           }}
@@ -36,26 +48,26 @@ export default function CarouselParent() {
             return (
               <SwiperSlide key={dataCar.mainH} className="h-full w-full flex ">
                 <p className="text-[16px]  text-[#C75C6F]  mb-[15px] font-medium">{dataCar.sub}</p>
-                <div className=" mx-auto flex flex-col gap-[30px]  ">
+                <div className=" mx-auto flex flex-col gap-[30px] w-full ">
                   <h1
-                    className="text-[25px] md:text-[32px] lg:text-[40px]
-                   xl:text-[46px]  leading-[60px] font-medium"
+                    className="text-[25px] md:text-[45px] lg:text-[55px]
+                   xl:text-[56px]  leading-[60px] font-medium w-full"
                   >
                     {dataCar.mainH}
                   </h1>
                   <p>{dataCar.mainP}</p>
                   <p>{dataCar.secondP}</p>
 
-                  <div>
+                  <div className="mt-[40px]">
                     <GlobalBtn name={"Read Case Study"} />
                   </div>
                   <div className=" flex my-[60px]  gap-[60px]">
                     <div className="flex flex-col ">
-                      <p>{dataCar.numPer}</p>
+                      <motion.p className="text-[50px] font-bold text-[#C75C6F]">{rounded}</motion.p>
                       <p>ROI increase</p>
                     </div>
                     <div className="flex flex-col">
-                      <p>{dataCar.num}</p>
+                      <motion.p className="text-[50px] font-bold text-[#C75C6F]">{roundedNumPer}</motion.p>
                       <p>Monthly Website Visits</p>
                     </div>
                   </div>
--- src/components/Carousel/dataCarousel.tsx
+++ src/components/Carousel/dataCarousel.tsx
@@ -1,26 +1,29 @@
 export const dataCarousel = [
   {
     sub: "Latest Project Showcase",
-    mainH: "Elegant E-commerce Platform",
-    mainP: "Revolutionizing online shopping with a modern and user-friendly experience.",
+    mainH: "Logo, identity & web design for Store.uz",
+    mainP:
+      "Revolutionizing online shopping with modern design. Check out the stunning and user-friendly experience.",
     secondP: "Check out the stunning design and seamless functionality.",
-    num: "60k",
-    numPer: "15%",
+    num: 60,
+    numPer: 15,
   },
   {
     sub: "Client Spotlight",
-    mainH: "Digital Marketing Solutions",
-    mainP: "Helping businesses grow with targeted digital marketing strategies.",
+    mainH: "Digital marketing for MyTaxi",
+    mainP:
+      "Driving business growth with digital marketing strategies. Explore how our expertise delivers results for clients.",
     secondP: "Discover how our expertise drives results for our clients.",
-    num: "48k",
-    numPer: "22%",
+    num: 48,
+    numPer: 22,
   },
   {
     sub: "Innovative Design Series",
     mainH: "Interactive Mobile App Interface",
-    mainP: "Breaking barriers with a cutting-edge mobile app design that captivates users.",
+    mainP:
+      "Breaking barriers with cutting-edge mobile app design. Explore the future of user experience in the digital landscape.",
     secondP: "Explore the future of user experience in the digital landscape.",
-    num: "52k",
-    numPer: "19%",
+    num: 52,
+    numPer: 19,
   },
 ];
