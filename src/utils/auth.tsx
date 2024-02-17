503021ac40d2edea66fb3f3974c806d638e30cf4
JavokhirTursunboev <jtursunboev@bk.ru>
2024-02-17 12:23:03 +0900

    Update social login buttons

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -1,5 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOption = {
   providers: [
@@ -10,6 +11,10 @@ export const authOption = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
     }),
   ],
 };
