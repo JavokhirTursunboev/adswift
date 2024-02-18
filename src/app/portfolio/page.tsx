afadc9fffc00f78ef6e478c5da8983e5f0c9d9a4
JavokhirTursunboev <jtursunboev@bk.ru>
2024-02-18 17:03:24 +0900

    Refactor portfolio page and card component

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,15 +1,55 @@
 "use client";
 import dynamic from "next/dynamic";
 // Dynamically import the Card component
-const Card = dynamic(() => import("@/components/Portfolio/Card"),{
-  loading: () => <p>loading...</p>
+
+interface PortfolioItem {
+  _id: string;
+  description: string;
+  color: string;
+  title: string;
+  img: string; // You might need to add other properties here based on your data structure
+  link: string;
+}
+
+const Card = dynamic(() => import("@/components/Portfolio/Card"), {
+  loading: () => <p>loading...</p>,
 });
-import { Projects } from "@/fakedata/data";
+// import { Projects } from "@/fakedata/data";
 import { useScroll } from "framer-motion";
-import { ReactNode, useEffect, useRef } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Lenis from "@studio-freight/lenis";
 
+// // server
+// const getData = async () => {
+//   const res = await fetch("http://localhost:3000/api/portfolio", {
+//     cache: "no-store",
+//   });
+//   if (!res.ok) {
+//     throw new Error("Failed");
+//   }
+//   return res.json();
+// };
+
+// // end server func
+
 export default function Page() {
+  const [portfolios, setPortfolios] = useState<PortfolioItem[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/portfolio")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Networking response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setPortfolios(data);
+      })
+      .catch((e) => {
+        console.error("error fetching portoflios", e);
+      });
+  }, []);
+
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
     target: container,
@@ -28,15 +68,19 @@ export default function Page() {
   });
   return (
     <main ref={container} className="mt-[50px] mb-[100vh] my-auto">
-      {Projects.map((project, i): ReactNode => {
-        const targetScale = 1 - (Projects.length - i) * 0.05;
+      {portfolios.map((item, i): ReactNode => {
+        const targetScale = 1 - (portfolios.length - i) * 0.05;
 
         return (
           <Card
-            key={`p_${i}`}
+            key={item._id}
             i={i}
             progress={scrollYProgress}
-            {...project}
+            img={item.img}
+            description={item.description}
+            color={item.color}
+            title={item.title}
+            link="not added " // add this line
             range={[i * 0.25, 1]}
             targetScale={targetScale}
           />
--- src/components/Portfolio/Card.tsx
+++ src/components/Portfolio/Card.tsx
@@ -2,7 +2,7 @@
 interface CardProps {
   title: string;
   description: string;
-  src: string;
+  img: string;
   link: string;
   color: string;
   i?: number;
@@ -18,7 +18,7 @@ export default function Card({
   i,
   title,
   description,
-  src,
+  img,
 
   color,
   progress,
@@ -45,21 +45,27 @@ export default function Card({
         className="bg-[color]
          flex
          flex-col relative lg:top-[-10] top-[-10%]
-          h-[500px] max-w-[1000px]
+          h-[500px] w-[1000px]
            rounded-[25px] p-[20px] lg:p-[50px]
             transform-origin-top"
       >
         {/* Image for Phone */}
         <div className=" md:hidden   relative w-full h-full rounded-[25px] overflow-hidden">
           <motion.div style={{ scale: imageScale }} className="w-full h-full">
-            <Image fill src={`/images/${src}`} alt="iamges" className="object-cover" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+            <Image
+              fill
+              src={img}
+              alt="iamges"
+              className="object-cover"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
           </motion.div>
         </div>
-        <h2 className="text-center text-[22px] lg:text-[28px] mt-[20px] ">{title}</h2>
+        <h2 className="text-center text-[22px] md:text-[28px] mt-[20px] ">{title}</h2>
         {/* body */}
-        <div className="flex flex-col lg:flex-row h-full lg:mt-[50px]  gap-[50px]">
-          <div className=" w-full lg:w-[40%] relative top-[10%]">
-            <p className="text-[13px] lg:text-[16px]">{description}</p>
+        <div className="flex flex-col md:flex-row h-full md:mt-[50px]  gap-[50px]">
+          <div className=" w-full md:w-[40%] relative top-[10%]">
+            <p className="text-[13px] md:text-[16px]">{description}</p>
             <span className="flex items-center gap-[5px] ">
               <a href="#" target="_blank" className="text-[12px] underline cursor-pointer">
                 See more
@@ -69,7 +75,13 @@ export default function Card({
 
           <div className="hidden md:block relative w-[60%] h-full rounded-[25px] overflow-hidden">
             <motion.div style={{ scale: imageScale }} className="w-full h-full">
-              <Image fill src={`/images/${src}`} alt="iamges" className="object-cover" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+              <Image
+                fill
+                src={img}
+                alt="iamges"
+                className="object-cover"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
             </motion.div>
           </div>
         </div>
