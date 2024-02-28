5fa2a8e37f02675bad9edfc4f82eb2569c90055d
JavokhirTursunboev <jtursunboev@bk.ru>
2024-02-28 13:05:54 +0900

    Add slugify package

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,11 +1,13 @@
 import prisma from "@/utils/connect";
 import { redirect } from "next/navigation";
+import slugify from "slugify"; // Import slugify library
 
 export default function Write() {
   async function writePost(formData: FormData) {
     "use server";
     const title = formData.get("title") as string;
     const desc = formData.get("desc") as string;
+    const slug = slugify(title, { lower: true });
 
     // create new record
 
@@ -13,6 +15,7 @@ export default function Write() {
       data: {
         title,
         desc,
+        slug,
       },
     });
     redirect("/posts");
@@ -28,6 +31,7 @@ export default function Write() {
           <p>Context</p>
           <input type="text" className="border border-black" name="desc" />
         </div>
+
         <button type="submit"> Save </button>
       </form>
     </div>
